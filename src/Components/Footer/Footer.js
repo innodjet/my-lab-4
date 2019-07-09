b473ceb5bbe0111206d72ba31225dd7980f595b0
NIK DJETELI <innodjet@gmail.com>
2019-07-09 14:12:59 -0500

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
           <h5 className="card-title"> Technologies used in this project: </h5>
           <p className="card-text"> React Framework (React Hooks)</p>
           <p className="card-text"> Front End: HTML 5, CSS 3, JavaScript, Bootstrap </p>
-          <p className="card-text"> Back End: Node.js, ExpressJS and MongoDb </p>
+          <p className="card-text"> Back End: Node.js, ExpressJS </p>
           <a href="https://github.com/innodjet/my-lab-4" 
              target="_blank" 
              className="btn btn-primary" 
